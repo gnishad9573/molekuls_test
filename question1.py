fcf1e5ee67227241a42399e86e5bb03a45f3d5c2
gnishad9573 <37542848+gnishad9573@users.noreply.github.com>
2020-10-21 20:02:47 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -13,9 +13,8 @@ def main(numbers: typing.List[int], target_sum: int):
             c = -(a+b)
             if c in hash_table and hash_table[c] > i and hash_table[c] > j+i+1:
                 result.add((a,b,c))
-                return "Triplet exists"
-            else:
-                return "Triplet does not exists"
+                return True
+            return False
 if __name__ == "__main__":
     assert (main([5, 4, 10, 7, 1, 9], 13) is True), "Triplet exists"
     assert (main([4, 2, 5, 9, 11, 23], 9) is False), "Triple does not exist"
